cceca410cccb714302ba70c0658e08d455828623
renukamrj <50427169+renukamrj@users.noreply.github.com>
2019-06-03 18:12:10 -0400

    Add files via upload

---
+++ online_library/inc/data.php
@@ -0,0 +1,169 @@
+<?php
+//Books*
+$catalog[101] = [
+  "title" => "The Jungle Book",
+  "img" => "img/media/jungle.jpg",
+ "genre" => "Self-help",
+ "format" => "Paperback",
+ "year" => 2016,
+ "category" => "Books",
+ "authors" => [
+     "Desmond Tutu",
+     "14th Dalai Lama",
+     "Douglas Abrams"
+ ],
+ "publisher" => "Penguin Publishing Group",
+ "isbn" => '9780399185045'
+];
+$catalog[102] = [
+  "title" => "David Copperfield",
+  "img" => "img/media/david.jpg",
+ "genre" => "Self-help",
+ "format" => "Paperback",
+ "year" => 2016,
+ "category" => "Books",
+ "authors" => [
+     "Charles Dickens"
+ ],
+ "publisher" => "Penguin Publishing Group",
+ "isbn" => '9780399185045'
+];
+$catalog[103] = [
+  "title" => "Satsang",
+  "img" => "img/media/mother.jpg",
+ "genre" => "Self-help",
+ "format" => "Paperback",
+ "year" => 2016,
+ "category" => "Books",
+ "authors" => [
+     "Guru Pandit"
+ ],
+ "publisher" => "Penguin Publishing Group",
+ "isbn" => '9780399185045'
+];
+$catalog[104] = [
+  "title" => "The Encyclopedia of Newyork",
+  "img" => "img/media/newyork.jpg",
+ "genre" => "Self-help",
+ "format" => "Paperback",
+ "year" => 2016,
+ "category" => "Books",
+ "authors" => [
+     "Zenith Jackson"
+ ],
+ "publisher" => "Penguin Publishing Group",
+ "isbn" => '9780399185045'
+];
+//Movies
+
+$catalog[201] = [
+ "title" => "Lion",
+ "img" => "img/media/lion.jpg",
+ "genre" => "Action,Adventure,Fantasy",
+ "format" => "DVD",
+ "year" => 2009,
+ "category" => "Movies",
+ "director" => "Dev Patel",
+ "writers" => [
+     "JDavid Wenham"
+
+ ],
+ "stars" => [
+     "Dev Patel",
+     "Rooney Mara",
+     "Nicole Kidman"
+ ]
+];
+//Movies*
+$catalog[202] = [
+ "title" => "Bride and Prejudice",
+ "img" => "img/media/bride.jpg",
+ "genre" => "Action,Adventure,Fantasy",
+ "format" => "DVD",
+ "year" => 2009,
+ "category" => "Movies",
+ "director" => "Lata mangeskar",
+ "writers" => [
+     "Lata mangeskar"
+
+ ],
+ "stars" => [
+     "Aishworya Rao",
+ ]
+];
+//Movies
+$catalog[203] = [
+ "title" => "Titanic",
+ "img" => "img/media/titanic.jpg",
+ "genre" => "Action,Adventure,Fantasy",
+ "format" => "DVD",
+ "year" => 2009,
+ "category" => "Movies",
+ "director" => "James Cameron",
+ "writers" => [
+     "James Cameron"
+
+ ],
+ "stars" => [
+     "Leonardo Dicaprio",
+ ]
+];
+//Movies*
+$catalog[204] = [
+ "title" => "Kuch kuch hota hai",
+ "img" => "img/media/kuch.jpg",
+ "genre" => "comedy,drama",
+ "format" => "DVD",
+ "year" => 2009,
+ "category" => "Movies",
+ "director" => "Karan Johar",
+ "writers" => [
+     "Karan Johar"
+
+ ],
+ "stars" => [
+     "Sharukh Khan",
+     "Rani Mukherjee",
+     "Sigourney Weaver"
+ ]
+];
+//Music*
+$catalog[301] = [
+ "title" => "BoyZone",
+ "img" => "img/media/boyzone.jpg",
+ "genre" => "Clasical",
+ "format" => "CD",
+ "year" => 2012,
+ "category" => "Music",
+ "artist" => "Ludwig van Beethoven"
+];
+//Music*
+$catalog[302] = [
+ "title" => "Backstreet boys",
+ "img" => "img/media/back.jpg",
+ "genre" => "Clasical",
+ "format" => "CD",
+ "year" => 2012,
+ "category" => "Music",
+ "artist" => "Ludwig van Beethoven"
+];
+//Music*
+$catalog[303] = [
+ "title" => "Spice girls",
+ "img" => "img/media/spice.jpg",
+ "genre" => "Clasical",
+ "format" => "CD",
+ "year" => 2012,
+ "category" => "Music",
+ "artist" => "Ludwig van Beethoven"
+];
+//Music*
+$catalog[304] = [
+ "title" => "Scorpion",
+ "img" => "img/media/scorpion.jpg",
+ "genre" => "Clasical",
+ "format" => "CD",
+ "year" => 2012,
+ "category" => "Music",
+ "artist" => "Ludwig van Beethoven"
+];
---
+++ online_library/inc/footer.php
@@ -0,0 +1,18 @@
+</div><!-- end content -->
+
+  <div class="footer">
+
+    <div class="wrapper">
+
+      <ul>
+        <li><a href="https://www.instagram.com/LaGuardiaCC/">Instagram</a></li>
+        <li><a href="https://www.facebook.com/LaGuardiaCC/">Facebook</a></li>
+      </ul>
+
+      <p>&copy;<?php echo date("Y"); ?> My Online Library</p>
+
+    </div>
+
+  </div>
+
+
